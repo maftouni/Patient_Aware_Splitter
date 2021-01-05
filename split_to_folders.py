d32cc6624ea920f8885f7a7f0c7971db56298b7b
Maede Maftouni <m.maftouni@gmail.com>
2021-01-05 16:05:43 -0500

    Update split_to_folders.py

--- split_to_folders.py
+++ split_to_folders.py
@@ -63,5 +63,4 @@ def splitter(df, group, stratify_by):
 df = pd.read_csv('sample_meta_data.csv')
 df['Patient ID'] = df['Patient ID'].astype(str)
 
-
-splitted_dictionary = splitter(df,'Patient ID','COVID-19 Infection')
+splitter(df,'Patient ID','COVID-19 Infection')
