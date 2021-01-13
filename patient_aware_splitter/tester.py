380ff90ecebf0c2f46522784de303ab3634eca30
Maede Maftouni <m.maftouni@gmail.com>
2021-01-13 17:20:00 -0500

    Update tester.py

--- patient_aware_splitter/tester.py
+++ patient_aware_splitter/tester.py
@@ -2,4 +2,8 @@ df = pd.read_csv('sample_meta_data.csv')
 df['Patient ID'] = df['Patient ID'].astype(str)
 
 
+#folders
+splitter(df,'Patient ID','COVID-19 Infection')
+
+# dictionary
 splitted_dictionary = splitter(df,'Patient ID','COVID-19 Infection')
