08992f47495ba06fa7c0e3a4750315c788fe1d2f
Maede Maftouni <m.maftouni@gmail.com>
2021-01-13 17:07:29 -0500

    Update split_into_dictionary.py

--- patient_aware_splitter/split_into_dictionary.py
+++ patient_aware_splitter/split_into_dictionary.py
@@ -76,9 +76,4 @@ def splitter(df, group, stratify_by,pickling = False):
     return  diction
     
    
-    
-df = pd.read_csv('sample_meta_data.csv')
-df['Patient ID'] = df['Patient ID'].astype(str)
 
-
-splitted_dictionary = splitter(df,'Patient ID','COVID-19 Infection')
